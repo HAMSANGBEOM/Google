14e104fd40dd49b7f5b39bb8f26ddd3f93a37862
HAMSANGBEOM <ctkdqja@naver.com>
2019-12-31 23:21:26 +0900

    google

---
+++ Google2.js
@@ -0,0 +1,28 @@
+var a = document.getElementById('header-1').href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox";
+
+var a = document.getElementById('header-2').href="https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl";
+
+var a = document.getElementById('low-1').href="https://ads.google.com/intl/ko_kr/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1";
+
+var a = document.getElementById('low-2').href="https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1#?modal_active=none";
+
+var a = document.getElementById('low-3').href="https://about.google/?utm_source=google-KR&utm_medium=referral&utm_campaign=hp-footer&fg=1";
+
+var a = document.getElementById('low-4').href="https://www.google.com/search/howsearchworks/?fg=1";
+
+var a = document.getElementById('low-5').href="https://policies.google.com/privacy?fg=1";
+
+var a = document.getElementById('low-6').href="https://policies.google.com/terms?fg=1";
+
+var lucky = document.getElementById('lucky');
+lucky.addEventListener('click', function(){
+          document.location.href=("https://www.google.com/doodles/");
+})
+
+var search = document.getElementById('button');
+search.addEventListener('click', function(){
+          document.getElementById('search').submit();
+})
+
+var form = document.getElementById('search').action=("https://www.google.com/search");
+var text = document.getElementById('search').method="get`"
